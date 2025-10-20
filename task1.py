8b6c56aa6e7fff4e672c07502d1806d8c77ba298
Denis <denisbliznyuk@gmail.com>
2025-10-20 22:11:14 +0300

    update

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 import pathlib
 
 
-def total_salary(path: str) -> tuple [int, int]:
+def total_salary(path: str) -> tuple[int, int]:
     # 1 If file does not exist, raise FileNotFoundError
     if not pathlib.Path(path).exists():
         raise FileNotFoundError(f"The file at {path} does not exist.")
@@ -14,16 +14,16 @@ def total_salary(path: str) -> tuple [int, int]:
 
         for line in all_lines:
             try:
-                name, salary = line.split(",") # Alex Korp,3000
+                name, salary = line.split(",")  # Alex Korp,3000
             except ValueError:
-                raise ValueError(f"Line '{line.strip()}' is not in the expected 'Name,Salary' format.")
+                raise ValueError(
+                    f"Line '{line.strip()}' is not in the expected 'Name,Salary' format."
+                )
             total_sum += int(salary.strip())
         print(total_sum)
 
         return (total_sum, int(total_sum / len(all_lines)))
 
 
-
 total, average = total_salary("files/salary_file.txt")
 print(f"Total salary {total}, Average salary {average}")
-
